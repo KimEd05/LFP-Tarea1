5e7b067f8d9e3694f4e05bf47f83b522cd8da7c2
KimEd05 <36649884+KimEd05@users.noreply.github.com>
2019-03-27 22:57:23 -0600

    Clases

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace _LFP_Tarea1_201700507
+{
+	partial class ventanaPrincipal
+	{
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		/// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código generado por el Diseñador de Windows Forms
+
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.lblMostrar = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.txtNumeros = new System.Windows.Forms.TextBox();
+            this.btnResultado = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Font = new System.Drawing.Font("Pristina", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.Location = new System.Drawing.Point(325, 64);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(98, 31);
+            this.btnGuardar.TabIndex = 0;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Font = new System.Drawing.Font("Pristina", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMostrar.Location = new System.Drawing.Point(325, 172);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(98, 31);
+            this.btnMostrar.TabIndex = 1;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // lblMostrar
+            // 
+            this.lblMostrar.AutoSize = true;
+            this.lblMostrar.Font = new System.Drawing.Font("Monotype Corsiva", 28.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMostrar.Location = new System.Drawing.Point(81, 154);
+            this.lblMostrar.MinimumSize = new System.Drawing.Size(150, 0);
+            this.lblMostrar.Name = "lblMostrar";
+            this.lblMostrar.Size = new System.Drawing.Size(150, 57);
+            this.lblMostrar.TabIndex = 3;
+            this.lblMostrar.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblMostrar.Click += new System.EventHandler(this.lblMostrar_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Monotype Corsiva", 10.2F, System.Drawing.FontStyle.Italic);
+            this.lblResultado.Location = new System.Drawing.Point(81, 284);
+            this.lblResultado.MinimumSize = new System.Drawing.Size(150, 0);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(150, 20);
+            this.lblResultado.TabIndex = 4;
+            this.lblResultado.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblResultado.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtNumeros
+            // 
+            this.txtNumeros.Font = new System.Drawing.Font("Monotype Corsiva", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumeros.Location = new System.Drawing.Point(83, 68);
+            this.txtNumeros.Name = "txtNumeros";
+            this.txtNumeros.Size = new System.Drawing.Size(143, 27);
+            this.txtNumeros.TabIndex = 5;
+            this.txtNumeros.TextChanged += new System.EventHandler(this.txtNumeros_TextChanged);
+            // 
+            // btnResultado
+            // 
+            this.btnResultado.Font = new System.Drawing.Font("Pristina", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnResultado.Location = new System.Drawing.Point(325, 280);
+            this.btnResultado.Name = "btnResultado";
+            this.btnResultado.Size = new System.Drawing.Size(98, 30);
+            this.btnResultado.TabIndex = 7;
+            this.btnResultado.Text = "Resultado";
+            this.btnResultado.UseVisualStyleBackColor = true;
+            this.btnResultado.Click += new System.EventHandler(this.btnResultado_Click);
+            // 
+            // ventanaPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 391);
+            this.Controls.Add(this.btnResultado);
+            this.Controls.Add(this.txtNumeros);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lblMostrar);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.btnGuardar);
+            this.Name = "ventanaPrincipal";
+            this.Text = "[LFP]Tarea1_201700507";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnGuardar;
+		private System.Windows.Forms.Button btnMostrar;
+		private System.Windows.Forms.Label lblMostrar;
+		private System.Windows.Forms.Label lblResultado;
+		private System.Windows.Forms.TextBox txtNumeros;
+		private System.Windows.Forms.Button btnResultado;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _LFP_Tarea1_201700507
+{
+	public partial class ventanaPrincipal : Form
+	{
+		public ventanaPrincipal()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void txtNumeros_TextChanged(object sender, EventArgs e)
+		{
+			
+		}
+
+		private void lblMostrar_Click(object sender, EventArgs e)
+		{
+		}
+
+		ListaCircular listaCircular = null;
+
+		private void btnGuardar_Click(object sender, EventArgs e)
+		{
+			guardarNumeros();
+
+            listaCircular = new ListaCircular();
+
+            for (int i = 0; i < guardarNumeros().Length; i++)
+            {
+                listaCircular.insertar(guardarNumeros()[i]);
+            }
+
+            lblMostrar.Text = " ";
+            lblResultado.Text = " ";
+		}
+
+		private int[] guardarNumeros()
+		{
+			String numeros = txtNumeros.Text;
+            string[] num = numeros.Split(',');
+			int[] listadoNumeros = new int[num.Length];
+
+			for (int i = 0; i < num.Length; i++)
+			{
+				listadoNumeros[i] = Int32.Parse(num[i]);
+			}
+
+			ordenamientoBurbuja(listadoNumeros);
+
+			return listadoNumeros;
+		}
+
+		private void btnResultado_Click(object sender, EventArgs e)
+		{
+            //lblResultado.Text = "" + listaCircular;
+            listaCircular.mostrarOrdenada(lblResultado);
+		}
+
+		public static void ordenamientoBurbuja(int[] listado)
+		{
+			int t;
+			for (int i = 1; i < listado.Length; i++)
+			{
+				for (int j = listado.Length - 1; j >= i; j--)
+				{
+					if (listado[j - 1] > listado[j]) 
+					{
+						t = listado[j - 1];
+						listado[j - 1] = listado[j];
+						listado[j] = t;
+					}
+				}
+			}
+		}
+
+		private void btnMostrar_Click(object sender, EventArgs e)
+		{
+            
+            listaCircular.mostrar(lblMostrar);
+		}
+	}
+
+	public class ListaCircular
+	{
+		private Nodo raiz;
+        private Nodo raizResultado;
+
+
+		class Nodo
+		{
+			public int valor;
+			public Nodo ant, sig;
+		}
+
+		public ListaCircular()
+		{
+			raiz = null;
+		}
+
+		public void insertar(int x)
+		{
+			Nodo nuevo = new Nodo();
+			nuevo.valor = x;
+			if (raiz == null)
+			{
+				nuevo.sig = nuevo;
+				nuevo.ant = nuevo;
+				raiz = nuevo;
+                raizResultado = nuevo;
+			}
+			else
+			{
+				Nodo ultimo = raiz.ant;
+				nuevo.sig = raiz;
+				nuevo.ant = ultimo;
+				raiz.ant = nuevo;
+				ultimo.sig = nuevo;
+			}
+		}
+
+		public void mostrar(Label lblMostrar)
+			
+		{
+			Nodo reconocer = raiz;
+			lblMostrar.Text = "" + reconocer.valor;
+			
+			raiz = reconocer.sig;
+		}
+
+		public void mostrarOrdenada(Label lblResultado)
+		{
+			Nodo reconocer = raizResultado;
+            String texto = " ";
+
+            do
+			{                
+                texto = texto + reconocer.valor + " ";
+               
+
+                if (reconocer.sig != raizResultado) {
+                    texto = texto + "- ";
+                }
+                reconocer = reconocer.sig;
+
+            } while (reconocer != raizResultado);
+
+            lblResultado.Text = texto;
+        }
+
+	}
+
+}
